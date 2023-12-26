136ced26e8b6edef30cf52238faecba9d51cb830
Jeremiah Olisa <31079573+jeremiah-olisa@users.noreply.github.com>
2023-12-26 08:46:53 +0100

    vue pwa config

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const viteConfig = {
+  pwa: {
+    name: "Tracka",
+    themeColor: "#fffff",
+    msTileColor: "#2C457B",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  },
+};
+
+
+export default viteConfig;
