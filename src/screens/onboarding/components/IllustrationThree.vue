79822065642753eaa7a8cd538d6dc98e0d6bf3ff
Jeremiah Olisa <31079573+jeremiah-olisa@users.noreply.github.com>
2023-12-29 00:15:37 +0100

    onboarding screens

---
+++ src/screens/onboarding/components/IllustrationThree.vue
@@ -0,0 +1,21 @@
+<template>
+  <img src="./../../../assets/onboarding-illustrations-3.png" :width="width" :height="height" alt="onboarding-illustrations-3">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'IllustrationThree',
+  components: {},
+  props: {
+    width: {
+      type: String || Number,
+      default: '374'
+    },
+    height: {
+      type: String || Number,
+      default: '356'
+    }
+  }
+})
+</script>
